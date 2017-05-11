49c3b5f9414333c4748fd63eca23384d9704554c
Leon <1506816370@qq.com>
2017-05-11 15:22:11 +0800

    1、项目创建 2、支持sql server

---
+++ AspNet_Session/sqlserver.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sqlserver.aspx.cs" Inherits="AspNet_Session.sqlserver" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        key:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        value:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="创   建" />
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="读   取" />
+&nbsp;<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="删   除" />
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lb_error" runat="server" Font-Bold="True" Font-Italic="False" Font-Names="微软雅黑" Font-Overline="False" Font-Size="Large" Font-Underline="False" ForeColor="Red" Visible="False"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ AspNet_Session/sqlserver.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNet_Session
+{
+    public partial class sqlserver : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text.Trim() != "")
+            {
+                Session[TextBox1.Text.Trim()] = TextBox2.Text.Trim();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text.Trim() != "")
+            {
+                if (Session[TextBox1.Text.Trim()] == null)
+                {
+                    TextBox2.Text = "null";
+                }
+                else
+                {
+                    TextBox2.Text = Session[TextBox1.Text.Trim()].ToString();
+                }
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text.Trim() != "")
+            {
+                Session[TextBox1.Text.Trim()] = null;
+            }
+        }
+
+        protected void ShowError(bool show,string msg)
+        {
+            if (show)
+            {
+                lb_error.Visible = true;
+                lb_error.Text = msg;
+            }
+            else
+            {
+                lb_error.Visible = false;
+            }
+        }
+    }
+}
