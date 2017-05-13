ae6951301dbc235331def7095cd2c67a7c0eb5a3
Leon <1506816370@qq.com>
2017-05-13 16:20:28 +0800

    1、支持stateserver

---
+++ SqlServer/发布/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SqlServer.Global" Language="C#" %>
---
+++ StateServer2/PartitionResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace StateServer2
+{
+    public class PartitionResolver : IPartitionResolver
+    {
+        private String[] partitions;
+
+        public void Initialize()
+        {
+            partitions = ConfigurationManager.AppSettings["StateServers"].Split(new char[] { ',' });
+        }
+
+        public String ResolvePartition(Object key)
+        {
+            String sid = key as string;
+            int partitionID = Math.Abs(sid.GetHashCode()) % partitions.Length;
+            Debug.WriteLine(string.Format("sessionID: {0}, session服务器: {1}", sid, partitions[partitionID]));
+            return partitions[partitionID];
+        }
+    }
+}
